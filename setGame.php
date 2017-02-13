23ac4b5dd8f255b989f20c5dbf509e62e9975c69
Tiffany <rosalie568@hotmail.com>
2017-02-12 22:30:54 -0600

    Fixed creating game page

--- setGame.php
+++ setGame.php
@@ -13,7 +13,8 @@
 	<body>
 		<div class="body"></div>
 		<div class="container">
-			<a href="logOut.php" class="button"><span><?php
+			<a href="logOut.php" class="button"><span>
+				<?php
 						include 'include.php';		//get values for database
 
 						$con = mysql_connect($dbhost, $dbuser, $dbpass);
@@ -26,7 +27,8 @@
 
 						$row2 = mysql_fetch_array($check);
 
-						echo $row2['firstName'];?>
+						echo $row2['firstName'];
+				?>
 						</span>Sign Out</a>
 			<div class="grad"></div>
 
@@ -36,54 +38,65 @@
 			<br>
 			<div class="login">
 				<form action="createGame.php" method="post">
-					<input type="text" class="some_class" value="" id="some_class_1" name="getTime" placeholder="Pick date and time " >
-					<br><br>
-					<select name="location">
-					  <option value="">--Select a location--</option>
-					  	<?php
+					<table>
+						<tr>
+							<td> <h3>Date and Time: </h3> </td>
+							<td><input type="datetime-local" class="some_class" id="some_class_1" name="getTime" /> </td>
+						</tr>
+						<tr>
+							<td colspan="2">
+								<select name="location">
+								  <option value="">--Select a location--</option>
+								  	<?php
 
-							//search query for locations
-							$locations = mysql_query("
-								SELECT * FROM gameLocations ") ;
+										//search query for locations
+										$locations = mysql_query("
+											SELECT * FROM gameLocations ") ;
 
-							while($row = mysql_fetch_array($locations) )
-							{
-						?>
-						  <option value="<?php echo $row['gameLocation']?>"> <?php echo $row['gameLocation']?> </option>
+										while($row = mysql_fetch_array($locations) )
+										{
+									?>
+									  <option value="<?php echo $row['gameLocation']?>"> <?php echo $row['gameLocation']?> </option>
 
-						<?php
-								;
-					 		}
-						?>
-					</select>
-					<button type="submit" value="create" name="create" id="create">Create A Game</button>
+									<?php
+											;
+								 		}
+									?>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2"> <button type="submit" value="create" name="create" id="create">Create A Game</button> </td>
+						</tr>
+					</table>
 				</form>
 			</div>
 		</body>
-<script src="./jquery.js"></script>
-<script src="./jquery.datetimepicker.js"></script>
-<script>
-	$('#datetimepicker').datetimepicker({
-		dayOfWeekStart : 1,
-		lang:'en',
-		startDate:	'2014/01/05'
-	});
-	$('#datetimepicker').datetimepicker({value:'2015/04/15 05:03',step:10});
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.datetimepicker.js"></script>
+	<script>
 
-	$('.some_class').datetimepicker();
-	$('.someclass2').timepicker();
-	$('#default_datetimepicker').datetimepicker({
-		formatTime:'H:i',
-		formatDate:'d.m.Y',
+		$('#datetimepicker').datetimepicker({
+			dayOfWeekStart : 1,
+			lang:'en',
+			startDate:	'2014/01/05'
+		});
+		$('#datetimepicker').datetimepicker({value:'2015/04/15 05:03',step:10});
 
-		defaultTime:'10:00',
-		timepickerScrollbar:false
-	});
+		$('.some_class').datetimepicker();
+		$('.someclass2').timepicker();
+		$('#default_datetimepicker').datetimepicker({
+			formatTime:'H:i',
+			formatDate:'d.m.Y',
 
-	$('#datetimepicker10').datetimepicker({
-		step:5,
-		inline:true
-	});
+			defaultTime:'10:00',
+			timepickerScrollbar:false
+		});
 
-</script>
+		$('#datetimepicker10').datetimepicker({
+			step:5,
+			inline:true
+		});
+
+	</script>
 </html>
